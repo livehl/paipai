7487e7f387ad515a2fcee12cbb8bf0845e3f8cb7
zixuan <livehl@126.com>
2016-09-09 12:08:27 +0800

    1.增加更新频率

--- src/test/scala/Test.scala
+++ src/test/scala/Test.scala
@@ -15,5 +15,6 @@ object Test {
 //    val entity = new ByteArrayEntity("""{"Borrowernumber":"24069614","UserId":0,"listingId":"19129600"}""".getBytes("UTF-8"))
 //    val v=NetTool.HttpPost("http://wirelessgateway.ppdai.com/Invest/BorrowerinfoService/Borrowerinfo",entity=entity)
 //    println(v._2)
+//    PaiPaiLoans.loanInfo(19286556)
   }
 }
